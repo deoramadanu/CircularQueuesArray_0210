9cd2a2e1188f9285f0daf519585c4d7421cdb877
deoramadanu <deo121005@gmail.com>
2025-06-10 11:50:36 +0700

    case 2

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -118,7 +118,12 @@ int main()
                 q.insert();
                 break;
             }
-  
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            
             
         
     
