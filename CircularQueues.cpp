0c7bbda6941c8095b6ec83f337648c8b7d47dec7
deoramadanu <deo121005@gmail.com>
2025-06-10 11:50:55 +0700

    case 3

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -123,6 +123,11 @@ int main()
                 q.remove();
                 break;
             }
+            case '3':
+            {
+                q.display();
+                break;
+            }
             
             
         
