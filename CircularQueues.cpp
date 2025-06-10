d17291d3d5c7c59bc2ada671f1f714594fa95a2d
deoramadanu <deo121005@gmail.com>
2025-06-10 11:51:55 +0700

    menambahkan default

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -132,7 +132,12 @@ int main()
             {
                 return 0;
             }
-            
+            default: 
+            {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
+           
             
         
     
